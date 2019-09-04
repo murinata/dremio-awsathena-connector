43dbee2077ac39588dfb8aab89b03348c1584832
Sathish Senathi <ssenathi@gmail.com>
2019-09-05 09:58:10 +1000

    generic jdbc update

--- src/main/java/com/dremio/exec/store/jdbc/conf/AwsAthenaConf.java
+++ src/main/java/com/dremio/exec/store/jdbc/conf/AwsAthenaConf.java
@@ -18,7 +18,7 @@ package com.dremio.exec.store.jdbc.conf;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
-
+import com.dremio.exec.catalog.conf.Secret;
 import com.dremio.exec.catalog.conf.DisplayMetadata;
 import com.dremio.exec.catalog.conf.NotMetadataImpacting;
 import com.dremio.exec.catalog.conf.SourceType;
@@ -40,20 +40,41 @@ public class AwsAthenaConf extends AbstractArpConf<AwsAthenaConf> {
   private static final String ARP_FILENAME = "arp/implementation/awsathena-arp.yaml";
   private static final ArpDialect ARP_DIALECT =
       AbstractArpConf.loadArpFile(ARP_FILENAME, (ArpDialect::new));
-  private static final String DRIVER = "com.simba.athena.jdbc42.Driver";
+ // private static final String DRIVER = "com.simba.athena.jdbc42.Driver";
 
-  @NotBlank
   @Tag(1)
-  @DisplayMetadata(label = "AWSRegion")
-  public String awsregion = "ap-southeast-2";
-
+  @DisplayMetadata(label="JDBC URL  - for custom JDBC Driver - Override other settings")
+  public String jdbcurl = "";
+ 
+  @NotBlank 
   @Tag(2)
-  @DisplayMetadata(label = "AWS Credential Provider")
-  public String awscredentialProvider = "com.simba.athena.amazonaws.auth.DefaultAWSCredentialsProviderChain"; 
+  @DisplayMetadata(label = "ARP YAML File path - Push down defintions")
+  public String yamlPath = "arp/implementation/awsathena-arp.yaml";
+
+  @Tag(7)
+  @DisplayMetadata(label = "User Id / Name")
+  public String username = "";
+
+  @Tag(8)
+  @DisplayMetadata(label = "Password")
+  @Secret
+  public String password = "";
 
   @NotBlank
   @Tag(3)
-  @DisplayMetadata(label = "S3 Ouputput Location - Full S3 Path s3://...")
+  @DisplayMetadata(label = "JDBC Driver Class - Default is AWS Athena")
+  public String DRIVER = "com.simba.athena.jdbc42.Driver";
+
+  @Tag(4)
+  @DisplayMetadata(label = "AWSRegion - Used for AWS Services")
+  public String awsregion = "ap-southeast-2";
+
+  @Tag(5)
+  @DisplayMetadata(label = "AWS Credential Provider - Default Athena Default Credential Provider")
+  public String awscredentialProvider = "com.simba.athena.amazonaws.auth.DefaultAWSCredentialsProviderChain"; 
+
+  @Tag(6)
+  @DisplayMetadata(label = "S3 Ouputput Location - Required for Athena - Full S3 Path s3://...")
   public String s3OutputLocation = "s3://temp/athena-output/";
   
   
@@ -65,7 +86,11 @@ public class AwsAthenaConf extends AbstractArpConf<AwsAthenaConf> {
 	if ("".equals(s3OutputLocation)) {
 		s3OutputLocation = "s3://temp/athena-output";
 	}
-    return String.format("jdbc:awsathena://AWSRegion=%s;AwsCredentialsProviderClass=%s;S3OutputLocation=%s",
+    if (!("".equals(jdbcurl))) {
+       return jdbcurl;
+    }
+
+    return String.format("jdbc:awsathena://AwsRegion=%s;S3OutputLocation=%s",
 	  awsregion,awscredentialProvider,s3OutputLocation);
   }
 
@@ -83,7 +108,7 @@ public class AwsAthenaConf extends AbstractArpConf<AwsAthenaConf> {
 
   private CloseableDataSource newDataSource() {
     return DataSources.newGenericConnectionPoolDataSource(DRIVER,
-      toJdbcConnectionString(), null, null, null, DataSources.CommitMode.DRIVER_SPECIFIED_COMMIT_MODE);
+      toJdbcConnectionString(), ("".equals(username))?null:password, ("".equals(password))?null:password, null, DataSources.CommitMode.DRIVER_SPECIFIED_COMMIT_MODE);
   }
 
   @Override
